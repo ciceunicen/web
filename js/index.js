c6e8a1e6e30c468391c98e2ef4b3362728cc4a8c
rocio <rociocpoppe@gmail.com>
2022-12-17 23:40:57 -0300

    se agrega funcionalidad de guardado de proyecto

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
-let title1 = id("title"),
-description1= id("description"), errorMsg = document.getElementsByClassName("error"),  
+let title = id("title"),
+description= id("description"), errorMsg = document.getElementsByClassName("error"),  
 successIcon = classes("success-icon"),
 failureIcon = classes("failure-icon");
 
@@ -17,29 +17,57 @@ let tecnica=document.querySelector("#tecnica");
 let aplicacionFinanciamiento=document.querySelector("#lineasFinanciamiento");
 let convocatoria=document.querySelector("#convocatoria");
 let otraAsistencia=document.querySelector("#otraAsistencia");
-
-document.querySelector("#save").addEventListener('click', (event) => {
-            let necesidadesCheckboxes = document.querySelectorAll('input[name="necesidadesCheckboxes"]:checked');
-            let output = [];
-            necesidadesCheckboxes.forEach((checkbox) => {
-                output.push(checkbox.value);
-            });
-            // alert(output);
-            let asistenciasCheckboxes = document.querySelectorAll('input[name="asistenciaCheckboxes"]:checked');
-            let output2 = [];
-            asistenciasCheckboxes.forEach((checkbox) => {
-                output2.push(checkbox.value);
-            });
-            // alert(output2);
-});    
-// Adjuntar archivos
-// Seleccionar 
+let necesidades=[];
+let asistencias = [];
+let estadios = [];
+let estadiosAProbar=null;
+   
+let attachments=[];
   
 document.getElementById("save").addEventListener("click", (e) => {
   e.preventDefault();
-  engine(title1, 0, "Ingrese un título al proyecto");
-  engine(description1, 1, "Ingrese una descripción al proyecto");
+  engine(title, 0, "Ingrese un título al proyecto");
+  engine(description, 1, "Ingrese una descripción al proyecto");
+  console.log(title.value);
+  title=title.value;
+  console.log(description.value);
+  description=description.value;
+  let necesidadesCheckboxes = document.querySelectorAll('input[name="necesidadesCheckboxes"]:checked');
+  necesidadesCheckboxes.forEach((checkbox) => {
+      necesidades.push(checkbox.value);
+      //console.log(necesidades);
+  });
+  let asistenciasCheckboxes = document.querySelectorAll('input[name="asistenciaCheckboxes"]:checked');
+  asistenciasCheckboxes.forEach((checkbox) => {
+      asistencias.push(checkbox.value);
+     // console.log(asistencias);
+  });
+    estadiosAProbar = document.querySelector('input[name="estadiosCheckboxes"]:checked').value;
+  // let estadiosCheckboxes = document.querySelectorAll('input[name="estadiosCheckboxes"]:checked');
 
+  // estadiosCheckboxes.forEach((checkbox) => {
+  //     estadios.push(checkbox.value);
+  //     console.log(estadios);
+  // });
+  saveAttachments();
+  // attachments.forEach((attachment)=>{
+  //   console.log(attachment);
+  // })
+  let datos={
+    "id_ProjectManager":2,
+    "title":title,
+    "description":description,
+    "stage":estadiosAProbar, ///deberia ser un array?o solo se puede elegir un estadio?--> se esta quedando con el último seleccionado, deberia no permitir elegir otro, deberia desmarcar la opcion anterio
+    "assitanceType":
+        asistencias,
+    "files":
+        attachments,
+    "needs":
+        necesidades,
+    "id_Admin":1
+  }
+  saveProject(datos);
+         
 });
 
 let engine = (id, serial, message) => {
@@ -58,5 +86,45 @@ let engine = (id, serial, message) => {
   }
 };
 
+document.querySelector(".iborrainputfile").addEventListener("click", saveAttachments);
+function saveAttachments(){
+  let inputs = document.getElementsByClassName("inputfile");
+	Array.prototype.forEach.call( inputs, function( input ){
+		let label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+      input.addEventListener('change', function( e ){
+			  let fileName = " ";
+			  if( this.files && this.files.length > 1 ){
+          fileName = (this.getAttribute('data-multiple-caption') || '' ).replace( '{count}', this.files.length );
+          for(let i=0;i<this.files.length;i++){
+            attachments.push(e.target.value.split( '\\' ).pop());
+        }
+        } else
+				  fileName = e.target.value.split( '\\' ).pop();
+			  if( fileName ){
+				  label.querySelector('span').innerHTML = fileName;
+        }
+			  else
+				  label.innerHTML = labelVal;
+		});
+	});
+}
 
+
+//POST
+
+
+
+async function saveProject(datos){
+  await fetch("http://localhost:8080/Project",{
+    method: "POST",
+    mode: 'cors',
+    body: JSON.stringify(datos),
+    headers: {"Access-Control-Allow-Origin":"*" ,},
+    headers: {"Content-type": "application/json; charset=UTF-8",}
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
+  //.catch(err => console.log(err));
+}
 
