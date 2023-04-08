67cab3d2a5db9b90f49bac482c49710958fc01e6
Cristian Marsico <cristianmarsico84@gmail.com>
2023-04-08 13:20:43 -0300

    carteles success y error
    se mejora el codigo

--- js/login.js
+++ js/login.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
     function checkInputs() {
         document.querySelectorAll('input').forEach(i => {
 
-
-
             i.onkeyup = () => {
                 if (checkSigninInput()) {
 
@@ -34,4 +32,30 @@ document.addEventListener("DOMContentLoaded", (e) => {
             return input.value.trim().length >= 1
         })
     }
+
+    document.getElementById("eye").addEventListener("click", (e) => {
+        e.preventDefault();
+        let input = document.getElementById('password-login')
+        let eye = document.getElementById('eye')
+        if (input.type == 'password') {
+            input.type = 'text'
+            eye.innerHTML = ' <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"></path><path d="M14.12 14.12a3 3 0 1 1-4.24-4.24"></path><path d="m1 1 22 22"></path><path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19"></path>'
+        } else {
+            input.type = 'password'
+            eye.innerHTML = '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><path d="M12 9a3 3 0 1 0 0 6 3 3 0 1 0 0-6z"></path>'
+        }
+    })
+
+    document.getElementById("btnReg").addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("first")
+        window.location.replace("http://localhost/proyectos/CICE/web/html/registro.html")
+    })
+
+    document.getElementById("btnLog").addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("login")
+
+    })
 })
+
