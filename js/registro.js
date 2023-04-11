d1f2ad598be92c02cbeb7aa2aec6dcf45013071b
Ezequiel Fernandez <ezzefernandezzz@gmail.com>
2023-04-11 02:12:55 -0300

    Se agrega nuevo campo de password de forma eficiente mediante DOM y se modifican algunos campos para checkeo HTML

--- js/registro.js
+++ js/registro.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", async(e) => {
         })
     }
 
-    document.getElementById("eye").addEventListener("click", () => {
+    /* document.getElementById("eye").addEventListener("click", () => {
         e.preventDefault();
         let input = document.getElementById('password')
         let eye = document.getElementById('eye')
@@ -48,7 +48,22 @@ document.addEventListener("DOMContentLoaded", async(e) => {
             input.type = 'password'
             eye.innerHTML = '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><path d="M12 9a3 3 0 1 0 0 6 3 3 0 1 0 0-6z"></path>'
         }
-    })
+    }) */
+
+    let btns_eyes = document.getElementsByClassName("eyes"); //HTML Collection
+    for (const eye of btns_eyes) {
+        eye.addEventListener("click", () => { //// PROBANDO, problema con selectorAll y byClasName debido a IN PROMISE!!!
+            let input = eye.previousElementSibling;
+            
+            if (input.type == 'password') {
+                input.type = 'text'
+                eye.innerHTML = ' <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"></path><path d="M14.12 14.12a3 3 0 1 1-4.24-4.24"></path><path d="m1 1 22 22"></path><path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19"></path>'
+            } else {
+                input.type = 'password'
+                eye.innerHTML = '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><path d="M12 9a3 3 0 1 0 0 6 3 3 0 1 0 0-6z"></path>'
+             }
+        });
+    }
 
     async function register() {
 
