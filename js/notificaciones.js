37fd8531120b05556e9b9e175eb9d8399d1bc300
renzo-guerra <renzoguerralatt@gmail.com>
2024-06-11 10:56:55 -0300

    Refactor: Nombres funciones sobre notificaciones
    Las funciones ya implementadas funcionan por id, sin importar si el
    usuario en cuestion es entrepeneur, les cambie el nombre para dar a
    entender que son genericas para los usuarios.

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,6 +1,6 @@
 const URLNotifications = "http://localhost:8080/notifications";
 
-async function getEntrepreneurNotifications(user) {
+async function getNotifications(user) {
   const response = await fetch(`${URLNotifications}/projectManager/${user.id}`, {
     "method": "GET",
     "headers": {
@@ -10,14 +10,13 @@ async function getEntrepreneurNotifications(user) {
   });
   if (response.ok) {
     const notificaciones = await response.json();
-    console.log("NOTIFICACIONES", notificaciones);
     return notificaciones;
   } else {
     console.error("Error al obtener las notificaciones");
   }
 }
 
-async function setEntrepreneurNotificationsAsRead(user) {
+async function setNotificationsAsRead(user) {
   const response = await fetch(`${URLNotifications}/projectManager/${user.id}`, {
     "method": "PUT",
     "headers": {
@@ -35,32 +34,34 @@ async function setEntrepreneurNotificationsAsRead(user) {
 }
 
 async function manageNotifications() {
+  // Seleccion de elementos HTML
   const notificacionesContainer = document.querySelector(".notificaciones-container");
   const iconoNotificacion = document.querySelector(".icono-notificacion");
 
-  if (notificacionesContainer != null && iconoNotificacion != null) {
-    let user = JSON.parse(localStorage.getItem('usuario'));
+  // Verificamos que ambos elementos hayan sido seleccionados del DOM
+  if(notificacionesContainer == null || iconoNotificacion == null){ return ;}
 
-    iconoNotificacion.addEventListener("click", async () => {
-        notificacionesContainer.classList.toggle("hidden");
-        if (notificacionesContainer.classList.contains("hidden")) {
-          const DTONotifications = await setEntrepreneurNotificationsAsRead(user);
-          showNotifications(DTONotifications.notifications);
+  let user = JSON.parse(localStorage.getItem('usuario'));
+  
+  iconoNotificacion.addEventListener("click", async () => {
+      notificacionesContainer.classList.toggle("hidden");
+      if (notificacionesContainer.classList.contains("hidden")) {
+        const DTONotifications = await setNotificationsAsRead(user);
+        showNotifications(DTONotifications.notifications);
 
-          if (DTONotifications.readQuantity > 0) {
-            iconoNotificacion.setAttribute("src", "../img/Icono-notificacion2.svg");
-          } else {
-            iconoNotificacion.setAttribute("src", "../img/Icono-notificacion1.svg");
-          }
+        if (DTONotifications.readQuantity > 0) {
+          iconoNotificacion.setAttribute("src", "../img/Icono-notificacion2.svg");
+        } else {
+          iconoNotificacion.setAttribute("src", "../img/Icono-notificacion1.svg");
         }
-    });
+      }
+  });
 
-    const DTONotifications = await getEntrepreneurNotifications(user);
-    showNotifications(DTONotifications.notifications);
+  const DTONotifications = await getNotifications(user);
+  showNotifications(DTONotifications.notifications);
 
-    if (DTONotifications.readQuantity > 0) {
-      iconoNotificacion.setAttribute("src", "../img/Icono-notificacion2.svg");
-    }
+  if (DTONotifications.readQuantity > 0) {
+    iconoNotificacion.setAttribute("src", "../img/Icono-notificacion2.svg");
   }
 }
 
@@ -94,27 +95,4 @@ function showNotifications(notifications) {
   }
 }
 
-// Muestra las notificaciones en el dashboard si el usuario es emprendedor y existe el dashboardHeader
-function showNotificationIcon() {
-    let user = JSON.parse(localStorage.getItem('usuario'));
-    if (user.rolType.toLowerCase() == "emprendedor") {
-      const dashboardHeader = document.querySelector(".dashboard-header");
-      if (dashboardHeader != null) {
-        dashboardHeader.innerHTML += `<img class="icono-notificacion" src="../img/Icono-notificacion1.svg" alt="notificacion">
-        <div class="notificaciones-container dashboard hidden">
-            <div class="notificaciones-top">
-                <p>Notificaciones</p>
-            </div>
-            <section class="notificaciones">
-
-            </section>
-            <div class="notificaciones-bottom">
-
-            </div>
-        </div>`
-      }
-  }
-}
-
-showNotificationIcon();
 manageNotifications();
