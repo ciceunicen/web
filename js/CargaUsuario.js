9f27f99d88b50a3657977ec0b1b7448b51b07b0c
Matias Sciancalepore <msciancalepore47@gmail.com>
2024-06-21 11:50:50 -0300

    nombre de usuario agregado a todas las pantallas

--- js/CargaUsuario.js
+++ js/CargaUsuario.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
     "use strict";
+    setTimeout(()=>{
+        addDatosUsuario();
+    },100);
 
-    let user_name = document.getElementById("name");
-    let user = JSON.parse(localStorage.getItem('usuario'));
-    let user_email=document.getElementById("mail");
-    
-    user_name.innerHTML=user.username;
-    user_email.innerHTML=user.email;
+
+
+    function addDatosUsuario(){
+            let user_name = document.getElementById("name");
+            let user = JSON.parse(localStorage.getItem('usuario'));
+            let user_email=document.getElementById("mail");
+            
+            user_name.innerHTML=user.username;
+            user_email.innerHTML=user.email;
+    }
 });
